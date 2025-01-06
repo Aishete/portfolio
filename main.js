477fc1531e4db684ca442b2c2974337f036576ed
Aishite <trybunhengy@gmail.com>
2025-01-06 14:57:16 +0700

    Upload from localfile
    Upload from localfile

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleColorSchemeIcon(element) {
+  const colorScheme = document.querySelector("html").dataset.colorScheme;
+  const svgElements = element.querySelectorAll("svg");
+  svgElements.forEach((svg) => {
+    svg.style.display = svg.classList.contains(colorScheme)
+      ? "inline-block"
+      : "none";
+  });
+}
